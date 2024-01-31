04394e3a9771d75b44f4dad727e41c8a9d402c51
ukp66482 <kartg0063810@gmail.com>
2024-01-31 23:06:00 +0800

    Update main.py

--- python code/main.py
+++ python code/main.py
@@ -50,15 +50,15 @@ def replaceZeroes(data):
     data[data == 0] = min_nonzero
     return data
 
-def read_images_from_folder(folder_path):
-    images = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(('.png', '.jpg', '.jpeg')):
-            img_path = os.path.join(folder_path, filename)
-            img = cv2.imread(img_path)
-            if img is not None:
-                images.append(img)
-    return images
+#def read_images_from_folder(folder_path):
+#    images = []
+#    for filename in os.listdir(folder_path):
+#        if filename.endswith(('.png', '.jpg', '.jpeg')):
+#            img_path = os.path.join(folder_path, filename)
+#            img = cv2.imread(img_path)
+#            if img is not None:
+#                images.append(img)
+#    return images
 
 #input_png_path = "./raw.png"
 #output_png_path = "./result.png"
